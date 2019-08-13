477c3161fd75431ea643389f96a9432935928f84
Nanayaku Kazuaki <sabanomisoni789@gmail.com>
2019-08-13 09:00:33 +0900

    sign up hold

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path, success: '登録に成功しました'
+      redirect_to root_path, success: '登録に成功しました'
     else
       flash.now[:danger] = "登録に失敗しました"
       render :new
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    # @user = User.find_by(id: params[:id])
   end
 
   private

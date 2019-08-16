ac7e0105033c340362bb7a74262012052a80066a
Nanayaku Kazuaki <sabanomisoni789@gmail.com>
2019-08-16 16:18:51 +0900

    prepare

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      redirect_to root_path, success: '登録に成功しました'
+      redirect_to @user , success: '登録に成功しました'
     else
       flash.now[:danger] = "登録に失敗しました"
       render :new
     end
   end
 
-  def show
-    @user = User.find_by(id: params[:id])
-    @microposts = @user.microposts
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private

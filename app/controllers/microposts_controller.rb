afe76e2ee056b53930e1c7a48fbdb134b5821bcb
Nanayaku Kazuaki <sabanomisoni789@gmail.com>
2019-08-16 12:57:17 +0900

    micropost save

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,18 +1,36 @@
 class MicropostsController < ApplicationController
+  def index
+    @microposts = Micropost.all
+  end
+
   def new
     @micropost = Micropost.new
   end
 
+  def show
+    @micropost = Micropost.find_by(id:params[:id])
+  end
+
   def create
-    @micropost = current_user.micropost.new(micropost_params)
+    @micropost = current_user.microposts.new(micropost_params)
 
     if @micropost.save
-      redirect_to micropost_path, success: '投稿しました'
+      redirect_to root_path, success: '投稿しました'
     else
       flash.now[:danger] = '投稿に失敗しました'
       render :new
     end
   end
+
+  def destroy
+    if @micropost.destroy
+      redirect_to :back, success: '投稿を削除しました'
+    else
+      flash.now[:danger] = '投稿の削除に失敗しました'
+      render :index
+    end
+  end
+
 
   private
   def micropost_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    @micropost = @user.micropost
   end
 
   private

5f1cdeb4fb918a7426c0ccaae0b972451da17aa2
Nanayaku Kazuaki <sabanomisoni789@gmail.com>
2019-08-17 18:56:57 +0900

    new-userpage-(id: current_user.id)-delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @current_user = @user
       redirect_to @user , success: '登録に成功しました'
     else
       flash.now[:danger] = "登録に失敗しました"

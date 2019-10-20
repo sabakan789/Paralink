b1d8d0c7814a4c337505be1d88565b9465127167
789サバ缶 <sabanomisoni789@gmail.com>
2019-10-20 21:56:53 +0900

    RSpec-user-model-refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,119 +1,105 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = FactoryBot.build(:user)
-  end
-
-  describe 'name,email,password,password_confirmation' do
-    context '全てに値が入っている場合' do
-      it 'tureを返す' do
-        expect(@user).to be_valid
-      end
-    end
-  end
-
   describe 'nameのバリデーション' do
+    let(:user){User.new(name:                     name,
+                        email:                   'test_name@example.com',
+                        password:                'password1234',
+                        password_confirmation:   'password1234') }
+    subject { user }
     context '空白の場合' do
-      it 'falseを返す' do
-        @user.name = ' '
-        expect(@user).to_not be_valid
-      end
+      let(:name) { ' ' }
+      it { is_expected.to_not be_valid }
     end
     context '1文字の場合' do
-      it 'tureを返す' do
-        @user.name = '@'
-        expect(@user).to be_valid
-      end
+      let(:name) { '@' }
+      it { is_expected.to be_valid }
     end
     context '15文字の場合' do
-      it 'tureを返す' do
-        @user.name = '@' * 15
-        expect(@user).to be_valid
-      end
+      let(:name) { '@' * 15 }
+      it { is_expected.to be_valid }
     end
     context '16文字の場合' do
-      it 'falseを返す' do
-        @user.name = '@' * 16
-        expect(@user).to_not be_valid
-      end
+      let(:name) { '@' * 16 }
+      it { is_expected.to_not be_valid }
     end
     context 'nameが重複していた場合' do
-      it 'falseを返す' do
-        FactoryBot.create(:user, name: 'test')
-        @user.name = 'test'
-        expect(@user).to_not be_valid
-      end
+      let!(:name_user){create(:user, name: 'test_name',email: 'name@example.com')}
+      let(:name) { 'test_name' }
+      it { is_expected.to_not be_valid }
     end
   end
 
   describe 'emailのバリデーション' do
+    let(:user){User.new(name:                    'test_email',
+                        email:                    email,
+                        password:                'password1234',
+                        password_confirmation:   'password1234') }
+    subject { user }
     context '空白の場合' do
-      it 'falseを返す' do
-        @user.email = ' '
-        expect(@user).to_not be_valid
-      end
+      let(:email) { ' ' }
+      it { is_expected.to_not be_valid }
     end
-    context 'emailが正規表現と違う場合' do
-      it 'falseを返す' do
-        expect(FactoryBot.build(:user, email: 'testtestcom')).to_not be_valid
-
-        expect(FactoryBot.build(:user, email: 'test@testcom')).to_not be_valid
-
-        expect(FactoryBot.build(:user, email: 'testtest.com')).to_not be_valid
-
-        expect(FactoryBot.build(:user, email: 'test@test,com')).to_not be_valid
-
-        expect(FactoryBot.build(:user, email: 'test@test@com')).to_not be_valid
-
-        expect(FactoryBot.build(:user, email: 'test.test.com')).to_not be_valid
-      end
+    context '「@」「.」がない場合' do
+      let(:email) { 'testtestcom' }
+      it { is_expected.to_not be_valid }
+    end
+    context '「.」がない場合' do
+      let(:email) { 'test@testcom' }
+      it { is_expected.to_not be_valid }
+    end
+    context '「@」がない場合' do
+      let(:email) { 'testtest.com' }
+      it { is_expected.to_not be_valid }
+    end
+    context '「.」が「,」の場合' do
+      let(:email) { 'test@test,com' }
+      it { is_expected.to_not be_valid }
+    end
+    context '「.」が「@」の場合' do
+      let(:email) { 'test@test@com' }
+      it { is_expected.to_not be_valid }
+    end
+    context '「@」が「.」の場合' do
+      let(:email) { 'test@test@com' }
+      it { is_expected.to_not be_valid }
     end
     context 'emailが重複していた場合' do
-      it 'falseを返す' do
-        FactoryBot.create(:user, email: 'test@test.com')
-        @user.email = 'test@test.com'
-        expect(@user).to_not be_valid
-      end
+      let!(:email_user){create(:user, name: 'test_email', email: 'test_email@example.com')}
+      let(:email) { 'test_email@example.com' }
+      it { is_expected.to_not be_valid }
     end
   end
 
   describe 'passwordのバリデーション' do
+    let(:user){User.new(name:                    'test_name',
+                        email:                   'test@example.com',
+                        password:                 password,
+                        password_confirmation:    password) }
+    subject { user }
     context 'アルファベットのみの場合' do
-      it 'falseを返す' do
-        @user.password = @user.password_confirmation = 'a' * 10
-        expect(@user).to_not be_valid
-      end
+      let(:password) { 'a' * 10 }
+      it { is_expected.to_not be_valid }
     end
     context '数字のみ場合' do
-      it 'falseを返す' do
-        @user.password = @user.password_confirmation = '1' * 10
-        expect(@user).to_not be_valid
-      end
+      let(:password) { '1' * 10 }
+      it { is_expected.to_not be_valid }
     end
     context '7文字の場合' do
-      it 'falseを返す' do
-        @user.password = @user.password_confirmation = '1' * 3 + 'a' * 4
-        expect(@user).to_not be_valid
-      end
+      let(:password) { '1' * 3 + 'a' * 4 }
+      it { is_expected.to_not be_valid }
     end
     context '8文字の場合' do
-      it 'tureを返す' do
-        @user.password = @user.password_confirmation = '1' * 4 + 'a' * 4
-        expect(@user).to be_valid
-      end
+      let(:password) { '1' * 4 + 'a' * 4 }
+      it { is_expected.to be_valid }
     end
     context '32文字の場合' do
-      it 'tureを返す' do
-        @user.password = @user.password_confirmation = '1' * 16 + 'a' * 16
-        expect(@user).to be_valid
-      end
+      let(:password) { '1' * 16 + 'a' * 16 }
+      it { is_expected.to be_valid }
     end
     context '33文字の場合' do
-      it 'turを返す' do
-        @user.password = @user.password_confirmation = '1' * 16 + 'a' * 17
-        expect(@user).to_not be_valid
-      end
+      let(:password) { '1' * 16 + 'a' * 17 }
+      it { is_expected.to_not be_valid }
     end
   end
 end
